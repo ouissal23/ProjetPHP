c6f6f8292f51056e87aaca47bd918df317e7f891
ouissal23 <ouissal.aboussaid@gmail.com>
2024-11-27 19:53:03 +0100

    first commmit

---
+++ createPOO.php
@@ -0,0 +1,153 @@
+<?php
+
+
+$emailValue = "";
+$lnameValue = "";
+$fnameValue = "";
+$passwordValue = "";
+$errorMesage = "";
+$successMesage = "";
+if(isset($_POST["submit"])){
+
+
+    $emailValue = $_POST["email"];
+    $lnameValue = $_POST["lastName"];
+    $fnameValue = $_POST["firstName"];
+    $passwordValue = $_POST["password"];
+
+
+    if(empty($emailValue) || empty($fnameValue) || empty($lnameValue) || empty($passwordValue)){
+
+
+            $errorMesage = "all fileds must be filed out!";
+
+
+    }else if(strlen($passwordValue) < 8 ){
+        $errorMesage = "password must contains at least 8 char";
+    }else if(preg_match("/[A-Z]+/", $passwordValue)==0){
+        $errorMesage = "password must contains  at least one capital letter!";
+    }else{
+    
+        include('connection.php');
+   
+
+
+    $connection= new Connection();
+  
+
+
+    $connection->selectDatabase('Lab');
+   
+    
+   include('Researcher.php');
+
+
+   $researcher = new Researcher($fnameValue,$lnameValue,$emailValue,$passwordValue);
+
+
+   $researcher->insertResearcher('Researcher',$connection->conn);
+
+   $successMesage = Researcher::$successMsg;
+
+   $errorMesage = Researcher::$errorMsg;
+
+
+
+
+
+$emailValue = "";
+$lnameValue = "";
+$fnameValue = "";   
+      
+ }
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container my-5 ">
+
+
+        <h2>SIGN UP</h2>
+
+
+    <?php
+
+
+    if(!empty($errorMesage)){
+  echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+  <strong>$errorMesage</strong>
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'>
+  </button>
+  </div>";
+    }
+       ?>
+
+
+        <br>
+        <form method="post">
+            <div class="row mb-3">
+                    <label class="col-form-label col-sm-1" for="fname">First Name:</label>
+                    <div class="col-sm-6">
+                        <input value="<?php echo $fnameValue ?>" class="form-control" type="text" id="fname" name="firstName">
+                    </div>
+            </div>
+            <div class="row mb-3">
+                    <label class="col-form-label col-sm-1" for="lname">Last Name:</label>
+                    <div class="col-sm-6">
+                        <input  value="<?php echo $lnameValue ?>" class="form-control" type="text" id="lname" name="lastName">
+                    </div>
+            </div>
+            <div class="row mb-3 ">
+                    <label class="col-form-label col-sm-1" for="email">Email:</label>
+                    <div class="col-sm-6">
+                        <input value=" <?php echo $emailValue ?>" class="form-control" type="email" id="email" name="email">
+                    </div>
+            </div>
+            <div class="row mb-3 ">
+                    <label class="col-form-label col-sm-1" for="password">Password:</label>
+                    <div class="col-sm-6">
+                        <input  class="form-control" type="password" id="password" name="password" >
+                    </div>
+            </div>
+
+
+            <?php
+            if(!empty($successMesage)){
+echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+<strong>$successMesage</strong>
+<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'>
+</button>
+</div>";
+            }
+  ?>  
+      
+
+
+            <div class="row mb-3">
+                    <div class="offset-sm-1 col-sm-3 d-grid">
+                        <button name="submit" type="submit" class=" btn btn-primary">Signup</button>
+                    </div>
+                    <div class="col-sm-1 col-sm-3 d-grid">
+                        <a class="btn btn-outline-primary" >Login</a>
+                    </div>
+            </div>
+        </form>
+
+
+    </div>
+
+
+</body>
+</html>
