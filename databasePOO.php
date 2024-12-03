0ddbedde79fab8dc72c671571511bfd9d0da47a6
ouissal23 <ouissal.aboussaid@gmail.com>
2024-12-03 20:57:30 +0100

    Création de la table Fields

--- databasePOO.php
+++ databasePOO.php
@@ -6,7 +6,15 @@ include('connection.php');
 $connection = new Connection();
 
 
-$connection->createDatabase('Lab');
+//$connection->createDatabase('Lab');
+
+$connection->selectDatabase('Lab');
+
+$query0 ="
+CREATE TABLE Fields(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
+) "; 
 
 $query = "
 CREATE TABLE Researcher (
@@ -15,14 +23,15 @@ firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
 email VARCHAR(50) UNIQUE,
 password VARCHAR(80),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)
-";
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+idField INT(6) UNSIGNED NOT NULL,
+FOREIGN KEY (idField) REFERENCES Fields(id)
+)";
 
 $connection->selectDatabase('Lab');
 
+$connection->createTable($query0,$query);
 
-$connection->createTable($query);
 
 
 ?>
